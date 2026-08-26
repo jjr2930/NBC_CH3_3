00995af8f2c7e9fa8bf8ae8c9cec6e8335af21cd
jjr2930 <joojungyeol@gmail.com>
2026-08-26 20:17:50 +0900

    [Add] 포인트 저장을 위한 게임 인스턴스 저장

---
+++ Source/NBC_CH3_3/Private/IngameGameInstance.cpp
@@ -0,0 +1,16 @@
+﻿#include "IngameGameInstance.h"
+
+void UIngameGameInstance::AddScore(int InAmount)
+{
+    Score += InAmount;
+}
+
+void UIngameGameInstance::ClearScore()
+{
+    Score = 0;
+}
+
+int UIngameGameInstance::GetScore()
+{
+    return Score;
+}
---
+++ Source/NBC_CH3_3/Public/IngameGameInstance.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "IngameGameInstance.generated.h"
+
+UCLASS()
+class NBC_CH3_3_API UIngameGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+    void AddScore(int InAmount);
+    void ClearScore();
+
+    int GetScore();
+
+protected:
+    int Score;
+};
