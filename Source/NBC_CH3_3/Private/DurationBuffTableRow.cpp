d8ce10f8193ac123807acee2b90e3bf6684de909
jjr2930 <joojungyeol@gmail.com>
2026-08-28 21:04:53 +0900

    [Add] 버프 테이블 작성, 바로적용 버프, 지속 적용 버프 테이블 만듬

---
+++ Source/NBC_CH3_3/Private/DurationBuffTableRow.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DurationBuffTableRow.h"
+
+DurationBuffTableRow::DurationBuffTableRow()
+{
+}
+
+DurationBuffTableRow::~DurationBuffTableRow()
+{
+}
---
+++ Source/NBC_CH3_3/Public/DurationBuffTableRow.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class NBC_CH3_3_API DurationBuffTableRow
+{
+public:
+	DurationBuffTableRow();
+	~DurationBuffTableRow();
+};
