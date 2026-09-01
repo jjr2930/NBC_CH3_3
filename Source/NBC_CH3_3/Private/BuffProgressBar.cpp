5445a4e9297157f8c6fe4dc69f9fb9594bdb5089
jjr2930 <joojungyeol@gmail.com>
2026-09-01 21:15:41 +0900

    [Add] Buff progress bar Blueprint asset and cpp code added

---
+++ Source/NBC_CH3_3/Private/BuffProgressBar.cpp
@@ -0,0 +1,47 @@
+﻿#include "BuffProgressBar.h"
+#include "JUtility.h"
+#include "Buff.h"
+
+#include <Components/ProgressBar.h>
+#include <Engine/Texture2D.h>
+#include <Components/Image.h>
+
+void UBuffProgressBar::BindBuff(FDurationBuff* Buff)
+{
+    DurationBuff = Buff;
+
+    SetTexture(DurationBuff->GetIconTexture());
+}
+
+bool UBuffProgressBar::IsMyBuff(FDurationBuff* Buff)
+{
+    if (nullptr == Buff)
+        return false;
+
+    return DurationBuff == Buff;
+}
+
+void UBuffProgressBar::SetPercent(float Percent)
+{
+    RemainTimeProgressBar->SetPercent(Percent);
+}
+
+void UBuffProgressBar::SetTexture(UTexture2D* IconTexture)
+{
+    Icon->SetBrushFromTexture(IconTexture);
+}
+
+void UBuffProgressBar::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+    JASSERT(nullptr != DurationBuff, "Duration Buff is nullptr");
+
+    float BuffStartTime = DurationBuff->GetStartTime();
+    float BuffDuration = DurationBuff->GetDuration();
+
+    float Now = (float)(GetWorld()->TimeSeconds);
+    float DuringTime = Now - BuffStartTime;
+    float Percent = DuringTime / BuffDuration;
+    Percent = 1.0f - Percent;
+
+    RemainTimeProgressBar->SetPercent(Percent);
+}
---
+++ Source/NBC_CH3_3/Public/BuffProgressBar.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BuffProgressBar.generated.h"
+
+class UProgressBar;
+class UTexture2D;
+class UImage;
+class FDurationBuff;
+
+UCLASS()
+class NBC_CH3_3_API UBuffProgressBar : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+    void BindBuff(FDurationBuff* Buff);
+    bool IsMyBuff(FDurationBuff* Buff);
+    void SetPercent(float Percent);
+    void SetTexture(UTexture2D* IconTexture);
+
+protected:
+    virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+    
+protected:
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UProgressBar> RemainTimeProgressBar;
+
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UImage> Icon;
+    
+    /*
+    * TODO: 스마트 포인터로 변환하기
+    */
+    FDurationBuff* DurationBuff;
+};
