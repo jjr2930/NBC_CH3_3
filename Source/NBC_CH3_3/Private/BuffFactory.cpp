26ba2ebf0f42f1d74f6054cd022a0ecc231b094d
jjr2930 <joojungyeol@gmail.com>
2026-09-01 15:15:51 +0900

    [Add] 버프 팩토리 추가.

---
+++ Source/NBC_CH3_3/Private/BuffFactory.cpp
@@ -0,0 +1,49 @@
+﻿#include "BuffFactory.h"
+#include "Buff.h"
+#include "BuffTableRowBase.h"
+#include "DurationBuffTableRow.h"
+#include "InstantBuffTableRow.h"
+#include "JUtility.h"
+
+#include <Engine/World.h>
+
+FBuffFactory::FBuffFactory()
+{
+}
+
+FBuffFactory::~FBuffFactory()
+{
+}
+
+FBuff* FBuffFactory::CreateBuff(UWorld* InWorld, const EBuffType BuffType, const FBuffTableRowBase* BuffData)
+{
+    switch (BuffType)
+    {
+    case EBuffType::Duration:
+    {
+        const FDurationBuffTableRow* DurationBuffTable = static_cast<const FDurationBuffTableRow*>(BuffData);
+        return  new FDurationBuff(DurationBuffTable->TargetStat
+            , DurationBuffTable->Operator
+            , DurationBuffTable->IsIntValue
+            , DurationBuffTable->IntValue
+            , DurationBuffTable->FloatValue
+            , DurationBuffTable->Duration
+            , InWorld->TimeSeconds);
+    }
+
+    case EBuffType::Instant:
+    {
+        const FInstantBuffTableRow* InstantBuffTable = static_cast<const FInstantBuffTableRow*>(BuffData);
+        return new FInstantBuff(InstantBuffTable->TargetStat
+            , InstantBuffTable->Operator
+            , InstantBuffTable->IsIntValue
+            , InstantBuffTable->IntValue
+            , InstantBuffTable->FloatValue);
+    }
+    default:
+        JASSERT_NULLPTR(false
+            , "BuffType %s notsupported"
+            ,  *GET_ENUM_STRING(EBuffType, BuffType));
+    }
+}
+
---
+++ Source/NBC_CH3_3/Public/BuffFactory.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Enums.h"
+
+class FBuff;
+class UWorld;
+struct FBuffTableRowBase;
+
+class NBC_CH3_3_API FBuffFactory
+{
+public:
+	FBuffFactory();
+	~FBuffFactory();
+
+    static FBuff* CreateBuff(UWorld* InWorld, const EBuffType buffType, const FBuffTableRowBase* BuffData);
+};
